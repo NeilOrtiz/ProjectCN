89e6c314c8f1b393036bb7f01d1161d6cb3dbdae
nao160030 <nao160030@utdallas.edu>
2018-04-15 05:57:26 -0500

    [correction] change frame from byte[] to char[]

--- src/network/Transport.java
+++ src/network/Transport.java
@@ -7,13 +7,18 @@ public class Transport {
 
     public Transport (Parent dad) {
         this.dad=dad;
-        this.secData=0;
+        this.secData=12;
     }
 
     public void transport_send_string(String srcId,String dstID, String msg){
-        byte[] frame = this.data_messages(srcId, dstID, msg);
+        char[] frame = this.data_messages(srcId, dstID, msg);
         //System.out.println("msg size: "+msg.getBytes().length);
         //this.printBytes(frame);
+
+        for (char x:frame) {
+            System.out.print(x);
+        }
+        System.out.println("");
 
     }
 
@@ -21,24 +26,47 @@ public class Transport {
         //TODO: transport_layer_msg()
     }
 
-    private byte[] data_messages(String srcId,String dstID, String msg){
+    private char[] data_messages(String srcId,String dstID, String msg){
 
+        char[] msgs=new char[30];
         byte[] frame=new byte[30];
         
-        frame[0]=100;
-        frame=this.pushBytes(frame, srcId, 1);//byte source id (from "0" up to "9")
-        frame=this.pushBytes(frame, dstID, 2);//byte destination id (from "0" up to "9") 
-        
-        if (secData<10) { //byte sequence number (from "00" to "99")
-            frame=this.pushBytes(frame, "0", 3);
-            frame=this.pushBytes(frame,Integer.toString(secData), 4); 
+        msgs[0]='d';
+        msgs[1]=srcId.charAt(0);
+        msgs[2]=dstID.charAt(0);
+        if (secData<10) {
+            msgs[3]='0';
+            msgs[4]=Integer.toString(secData).charAt(0) ;
         } else {
-            frame=this.pushBytes(frame,Integer.toString(secData), 3);
+            double part=secData/10d;
+            long iPart=(long)part;
+            int  fPart=(int)Math.ceil(((part-(double)iPart)*10d));
+            msgs[3]=Long.toString(iPart).charAt(0) ;
+            msgs[4]=Integer.toString(fPart).charAt(0);
         }
 
-        frame=this.pushBytes(frame,msg,5);
+        char[] msg_arr=msg.toCharArray();
 
-        return frame;
+        int counter=5;
+        for (char x:msg_arr) {
+            msgs[counter]=x;
+            counter++;
+        }
+
+        // frame[0]=100;
+        // frame=this.pushBytes(frame, srcId, 1);//byte source id (from "0" up to "9")
+        // frame=this.pushBytes(frame, dstID, 2);//byte destination id (from "0" up to "9") 
+        
+        // if (secData<10) { //byte sequence number (from "00" to "99")
+        //     frame=this.pushBytes(frame, "0", 3);
+        //     frame=this.pushBytes(frame,Integer.toString(secData), 4); 
+        // } else {
+        //     frame=this.pushBytes(frame,Integer.toString(secData), 3);
+        // }
+
+        // frame=this.pushBytes(frame,msg,5);
+
+        return msgs;
     }
 
     private void ack_messages() {
