66ec631d4651be9883839315e800c49306b346fc
nao160030 <nao160030@utdallas.edu>
2018-04-15 05:58:42 -0500

    [correction] change frame from byte[] to char[] 2

---
+++ src/network/Networks.java
@@ -0,0 +1,14 @@
+package network;
+
+public class Networks {
+    private Parent dad;
+
+
+    public Networks (Parent dad) {
+        this.dad=dad;
+    }
+
+    public void network_receive_from_transport(){
+        //TODO: Networks.network_receive_from_transport()
+    }
+}
