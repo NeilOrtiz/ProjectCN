63b4021b724bf1f990a0bb0830f4f62dba6313f8
nao160030 <nao160030@utdallas.edu>
2018-04-17 16:36:04 -0500

    [creation] Writer.java

--- src/network/Datalink.java
+++ src/network/Datalink.java
@@ -8,6 +8,10 @@ public class Datalink {
     }
 
     public void datalink_receive_from_network(byte[] msg, int len, int next_hop) {
+        char dest=(char) msg[1];
+        System.out.println("[Datalink] dest: "+dest);
+
+        String fileName=".//"+"from"+dad.myID+"to"+dest+".txt";
 
     }
 }
---
+++ src/network/Writer.java
@@ -0,0 +1,13 @@
+package network;
+
+public class Writer {
+    private int count;
+
+    public Writer() {
+
+    }
+
+    public void writeFile() {
+        
+    }
+}
