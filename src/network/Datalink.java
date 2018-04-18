925d2dde0d5c4b2e627da0ff2d2ce96bb4db2c30
nao160030 <nao160030@utdallas.edu>
2018-04-18 01:42:03 -0500

    [implementation] byte-stuffing

--- src/network/Datalink.java
+++ src/network/Datalink.java
@@ -9,12 +9,49 @@ public class Datalink {
 
     public void datalink_receive_from_network(byte[] msg, int len, int next_hop) {
         char dest=(char) msg[1];
-        System.out.println("[Datalink] dest: "+dest);
+        String type;
+        
 
         String fileName=".//"+"from"+dad.myID+"to"+dest+".txt";
         Writer write = new Writer(fileName);
 
-        write.writeFile();
+        char tp=(char) msg[6];
+        if (tp=='d') {
+            type="data";
+        } else {
+            type="ack";
+        }
 
+        char secNum1= ((char) msg[2]);
+        char secNum2= ((char) msg[3]);
+        String secNum=Character.toString(secNum1)+Character.toString(secNum2);
+        char[] ntwMsg=this.getntwMsg(msg);
+        String msgStuffing=this.stuffing(ntwMsg);
+        write.writeFile(type,0,secNum,msgStuffing);
+    }
+
+    public char[] getntwMsg(byte[] msg){
+        char[] temp=new char[msg.length-6];
+        int counter=0;
+        for (int i=6;i<=msg.length-1;i++) {
+            temp[counter]=(char) msg[i];
+            counter++;
+        }
+        return temp;
+    }
+
+    public String stuffing(char[] ntwMsg) {
+        String temp="F";
+
+        for (char x:ntwMsg) {
+            if (x=='F'||x=='E'||x=='f'||x=='e'||x=='X'||x=='x') {
+                temp=temp+"X"+x;
+            } else {
+                temp=temp+x;
+            }
+        }
+        temp=temp+"E";
+
+        return temp;
     }
 }
--- src/network/Writer.java
+++ src/network/Writer.java
@@ -1,35 +1,23 @@
 package network;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 
 public class Writer {
-    private int count;
     private String pathname;
 
     public Writer(String pathname) {
         this.pathname=pathname;
-        this.count=0;
-
-        try {
-            File channel = new File(pathname);
-            FileWriter channelFile = new FileWriter(channel);
-            channelFile.close();
-        } catch (Exception e) {
-            System.err.println(e+" in writer");
-        }
     }
 
-    public void writeFile() {
+    public void writeFile(String type, int channel, String secNum, String msg) {
 
         try {
-            String str="Count " + count;
+            String str=type+" "+channel+" "+secNum+" "+msg;
             BufferedWriter WriteFile = new BufferedWriter(new FileWriter(this.pathname,true));
+            WriteFile.write("\r\n");
             WriteFile.write(str);
-            WriteFile.write("\n");
             WriteFile.close();
-            this.count++;
         } catch (Exception e) {
             System.err.println(e+" in writer");
         }
